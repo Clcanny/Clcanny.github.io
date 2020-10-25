0ec5ce095d41d3ddc9c5b6e3369a73ef1c5071fd
Charles Ruan <a837940593@gmail.com>
2020-10-25 14:56:41 +0800

    (modify) (config files) (Update npm plugins and use markdown style to render markdown files.)

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-npm install hexo
-
-node_modules/hexo/bin/hexo init binary
-cd binary
-cp ../config/package.json package.json
-npm install
+npm install --save
 git clone https://github.com/iissnan/hexo-theme-next themes/next
-cp ../config/next.yml themes/next/_config.yml
-cp ../config/site_settings.yml _config.yml
-../node_modules/hexo/bin/hexo generate
+node_modules/hexo/bin/hexo generate
 # ../node_modules/hexo/bin/hexo server
