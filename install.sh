c6ffbd5e81ea45ae08deb76b6bbb7ada2c59ab38
Charles <a837940593@gmail.com>
2020-11-22 20:04:17 +0800

    (modify) (install.sh) (Change next repository.)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm install --save
-git clone https://github.com/iissnan/hexo-theme-next themes/next
+git clone https://github.com/theme-next/hexo-theme-next themes/next
 node_modules/hexo/bin/hexo generate
 # node_modules/hexo/bin/hexo server
